cc1a64def060bc6ca7ef5f61a961d7c73356d640
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-10-04 22:55:52 -0400

    Update download.sh

--- patch/download.sh
+++ patch/download.sh
@@ -1,8 +1,4 @@
 
-MAGISKVERSION="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.version")"
-MAGISKLINK="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.link")"
-echo "Magisk stable version is $MAGISKVERSION"
-
 wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-release.apk -O canary.apk
 wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-debug.apk -O debug.apk
 wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-release.apk -O canary-huskydg.apk
