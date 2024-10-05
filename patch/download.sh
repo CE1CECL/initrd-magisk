fd7f281b35ba955d370f3727bef72660e7bad0cb
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2024-10-04 22:55:43 -0400

    Update download.sh

--- patch/download.sh
+++ patch/download.sh
@@ -3,8 +3,8 @@ MAGISKVERSION="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-file
 MAGISKLINK="$(wget -q https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json -O - | jq -r ".magisk.link")"
 echo "Magisk stable version is $MAGISKVERSION"
 
-wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-release.apk -O canary.apk
-wget https://raw.githubusercontent.com/topjohnwu/magisk-files/canary/app-debug.apk -O debug.apk
-wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-release.apk -O canary-huskydg.apk
-wget https://raw.githubusercontent.com/HuskyDG/magisk-files/main/app-debug.apk -O debug-huskydg.apk
-wget $MAGISKLINK -O stable.apk
+wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-release.apk -O canary.apk
+wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-debug.apk -O debug.apk
+wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-release.apk -O canary-huskydg.apk
+wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-debug.apk -O debug-huskydg.apk
+wget https://raw.githubusercontent.com/CE1CECL/initrd-magisk/refs/heads/main/app-release.apk -O stable.apk
